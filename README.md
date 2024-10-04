e1d2664abb34e89a6f28e598788cc68983d9c96a
Pineapple <126540704+IsThisShadow@users.noreply.github.com>
2024-10-04 13:51:00 -0400

    Main
    This is the main game, it doesnt work but i have everything coded, i just don't have the time to find it.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TowerDefenseTemplate
- 
+ I used the map layout from markyJoe's SRPG tileset on itch
+and the enemys from LuizMelo's Monsters Creatures fantasy on itch
---
+++ Scenes/Bat.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed = 100
+var Health = 10
+func _process(delta):
+	get_parent().set_progress(get_parent().get_progress() + speed*delta)
+
+	if get_parent().get_progress_ratio() == 1:
+		Game.Health -= 1
+		death()
+
+	if Health <= 0:
+		Game.Gold += 1
+		death()
+	
+func death():
+	get_parent().get_parent().queue_free()
